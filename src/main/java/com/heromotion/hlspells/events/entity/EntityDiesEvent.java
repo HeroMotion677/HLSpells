5ab11d799ecaf3f009d8c9f3c246944f58626094
Max094Reikeb <maxencebekier@gmail.com>
2021-06-05 18:27:59 +0200

    Modified Totem of Griefing

--- src/main/java/com/heromotion/hlspells/events/entity/EntityDiesEvent.java
+++ src/main/java/com/heromotion/hlspells/events/entity/EntityDiesEvent.java
@@ -7,6 +7,7 @@ import com.heromotion.hlspells.network.packets.TotemPacket;
 
 import net.minecraft.entity.player.*;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
 import net.minecraft.world.Explosion;
 
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
@@ -24,13 +25,18 @@ public class EntityDiesEvent {
         if (event.getEntity() instanceof PlayerEntity) {
             if (!event.getSource().isBypassInvul()) {
                 PlayerEntity player = (PlayerEntity) event.getEntity();
+
+                if (!(player.getMainHandItem().getItem().equals(ItemInit.TOTEM_OF_GRIEFING.get())
+                        || player.getOffhandItem().getItem().equals(ItemInit.TOTEM_OF_GRIEFING.get()))) return;
+                Hand hand = (player.getMainHandItem().getItem().equals(ItemInit.TOTEM_OF_GRIEFING.get()) ? Hand.MAIN_HAND : Hand.OFF_HAND);
+
+                player.setItemInHand(hand, ItemStack.EMPTY);
+                player.inventory.setChanged();
+
                 for (int i = 0; i < player.inventory.getContainerSize(); i++) {
-                    if (player.inventory.getItem(i).getItem() == ItemInit.TOTEM_OF_GRIEFING.get()) {
-                        player.inventory.getItem(i).shrink(1);
-                        player.level.broadcastEntityEvent(player, (byte) 35);
-                        NetworkManager.INSTANCE.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity) player), new TotemPacket(new ItemStack(ItemInit.TOTEM_OF_GRIEFING.get())));
-                        player.level.explode(player, player.getX(), player.getY(), player.getZ(), 10.0F, Explosion.Mode.BREAK);
-                    }
+                    player.level.broadcastEntityEvent(player, (byte) 35);
+                    NetworkManager.INSTANCE.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity) player), new TotemPacket(new ItemStack(ItemInit.TOTEM_OF_GRIEFING.get())));
+                    player.level.explode(player, player.getX(), player.getY(), player.getZ(), 10.0F, Explosion.Mode.BREAK);
                 }
             }
         }
