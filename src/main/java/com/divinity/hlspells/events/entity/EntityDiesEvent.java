392cbd60c29569ad9c10562bd9308424e7999e5b
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2021-07-29 23:24:04 -0400

    Started fixing up totems and added new totem

--- src/main/java/com/divinity/hlspells/events/entity/EntityDiesEvent.java
+++ src/main/java/com/divinity/hlspells/events/entity/EntityDiesEvent.java
@@ -1,53 +1,59 @@
 package com.divinity.hlspells.events.entity;
 
 import com.divinity.hlspells.init.ItemInit;
-import com.divinity.hlspells.network.NetworkManager;
-import com.divinity.hlspells.network.packets.TotemPacket;
+import com.divinity.hlspells.items.ModTotemItem;
 import com.divinity.hlspells.HLSpells;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.Explosion;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.network.PacketDistributor;
+import net.minecraftforge.fml.server.ServerLifecycleHooks;
 
 @Mod.EventBusSubscriber(modid = HLSpells.MODID)
 public class EntityDiesEvent {
 
     @SubscribeEvent
     public static void onEntityDies(LivingDeathEvent event) {
-        if (event == null) return;
-
-        if (event.getEntity() instanceof PlayerEntity) {
-            if (!event.getSource().isBypassInvul()) {
-                PlayerEntity player = (PlayerEntity) event.getEntity();
-
-                if (!(player.getMainHandItem().getItem().equals(ItemInit.TOTEM_OF_GRIEFING.get())
-                        || player.getOffhandItem().getItem().equals(ItemInit.TOTEM_OF_GRIEFING.get())
-                        || player.getMainHandItem().getItem().equals(ItemInit.TOTEM_OF_RETURNING.get())
-                        || player.getOffhandItem().getItem().equals(ItemInit.TOTEM_OF_RETURNING.get()))) return;
-
-                Item item = (player.getMainHandItem().getItem().equals(ItemInit.TOTEM_OF_GRIEFING.get())
-                        || player.getOffhandItem().getItem().equals(ItemInit.TOTEM_OF_GRIEFING.get()))
-                        ? ItemInit.TOTEM_OF_GRIEFING.get() : ItemInit.TOTEM_OF_RETURNING.get();
-                Hand hand = (player.getMainHandItem().getItem().equals(item) ? Hand.MAIN_HAND : Hand.OFF_HAND);
-
-                if (item.equals(ItemInit.TOTEM_OF_GRIEFING.get())) {
-                    player.setItemInHand(hand, ItemStack.EMPTY);
-                    player.inventory.setChanged();
-                    player.level.explode(player, player.getX(), player.getY(), player.getZ(), 10.0F, Explosion.Mode.BREAK);
-                    player.level.broadcastEntityEvent(player, (byte) 35);
-                    NetworkManager.INSTANCE.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity) player), new TotemPacket(new ItemStack(item)));
-
-                } else if (item.equals(ItemInit.TOTEM_OF_RETURNING.get())) {
-                    player.getItemInHand(hand).getOrCreateTag().putString("registryKey", player.level.dimension().location().toString());
-                    player.getItemInHand(hand).getOrCreateTag().putDouble("dX", player.getX());
-                    player.getItemInHand(hand).getOrCreateTag().putDouble("dY", player.getY());
-                    player.getItemInHand(hand).getOrCreateTag().putDouble("dZ", player.getZ());
+        LivingEntity player = event.getEntityLiving();
+        World world = player.level;
+        if (event.getEntityLiving() instanceof PlayerEntity) {
+            for (Hand hand : Hand.values()) {
+                ItemStack heldItem = player.getItemInHand(hand);
+                // TOTEM OF GRIEFING
+                if (heldItem.getItem() == ItemInit.TOTEM_OF_GRIEFING.get()) {
+                    ModTotemItem.vanillaTotemBehavior(event, player, heldItem, ItemInit.TOTEM_OF_GRIEFING.get());
+                    world.explode(player, player.getX(), player.getY(), player.getZ(), 10.0F, Explosion.Mode.BREAK);
+                }
+                // TOTEM OF ESCAPING
+                if (heldItem.getItem() == ItemInit.TOTEM_OF_ESCAPING.get()) {
+                    ModTotemItem.vanillaTotemBehavior(event, player, heldItem, ItemInit.TOTEM_OF_ESCAPING.get());
+                    player.addEffect(new EffectInstance(Effects.INVISIBILITY, 200, 0));
+                    // teleport the player randomly nearby
+                    for(int i = 0; i < 16; ++i) {
+                        double xRand = player.getX() + (player.getRandom().nextDouble() - 0.5D) * 16.0D;
+                        double yRand = MathHelper.clamp(player.getY() + (double) (player.getRandom().nextInt(16) - 8), 0.0D, (double) (world.getHeight() - 1));
+                        double zRand = player.getZ() + (player.getRandom().nextDouble() - 0.5D) * 16.0D;
+                        player.randomTeleport(xRand, yRand, zRand, true);
+                        world.playSound((PlayerEntity) null, player.getX(), player.getY(), player.getZ(), SoundEvents.CHORUS_FRUIT_TELEPORT, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                        player.playSound(SoundEvents.CHORUS_FRUIT_TELEPORT, 1.0F, 1.0F);
+                    }
+                }
+                // TOTEM OF RETURNING
+                if (heldItem.getItem() == ItemInit.TOTEM_OF_RETURNING.get()) {
+                    ModTotemItem.vanillaTotemBehavior(event, player, heldItem, ItemInit.TOTEM_OF_RETURNING.get());
+                    // not functional yet, still cleaning up
                 }
             }
         }
---
+++ src/main/java/com/divinity/hlspells/items/ModTotemItem.java
@@ -0,0 +1,43 @@
+package com.divinity.hlspells.items;
+
+import com.divinity.hlspells.HLSpells;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Rarity;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+
+public class ModTotemItem extends Item {
+    public ModTotemItem() {
+        super(new Item.Properties().tab(ItemGroup.TAB_COMBAT).stacksTo(1).rarity(Rarity.UNCOMMON));
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public boolean isFoil(ItemStack itemstack) {
+        return true;
+    }
+
+    public static void vanillaTotemBehavior(LivingDeathEvent event, LivingEntity entity, ItemStack heldItem, Item animationItem) {
+        event.setCanceled(true);
+        heldItem.shrink(1);
+        entity.setHealth(1.0F);
+        entity.removeAllEffects();
+        entity.addEffect(new EffectInstance(Effects.REGENERATION, 900, 1));
+        entity.addEffect(new EffectInstance(Effects.ABSORPTION, 100, 1));
+        Minecraft.getInstance().particleEngine.createTrackingEmitter(entity, ParticleTypes.TOTEM_OF_UNDYING, 30);
+        entity.level.playSound((PlayerEntity) null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.TOTEM_USE, SoundCategory.PLAYERS, 1.0F, 1.0F);
+        entity.playSound(SoundEvents.TOTEM_USE, 1.0F, 1.0F);
+        Minecraft.getInstance().gameRenderer.displayItemActivation(new ItemStack(animationItem));
+    }
+}
