26fd8aafee2761a6d8fec5bbf68975c41cf4146c
jnaxv <jasonlovesme123@gmail.com>
2021-08-23 12:53:00 -0400

    Created Wizard armor Material, modified tick delay/xp cost with it.

--- src/main/java/com/divinity/hlspells/blocks/CustomFrostedIce.java
+++ src/main/java/com/divinity/hlspells/blocks/CustomFrostedIce.java
@@ -6,8 +6,8 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class CustomFrostedIce extends FrostedIceBlock {
-    public CustomFrostedIce(Properties p_i48394_1_) {
-        super(p_i48394_1_);
+    public CustomFrostedIce(Properties properties) {
+        super(properties);
     }
 
     @Override
