66f61ab162d60e6c16dd93faa5197bf8455eb8a0
Bryan Williams <bryanw082003@gmail.com>
2025-12-06 02:42:53 -0500

    Fixed Orb of Enchanting losing xp on game/server restart

---
+++ src/main/java/com/divinity/hlspells/world/blocks/blockentities/OrbOfEnchantingBE.java
@@ -0,0 +1,47 @@
+package com.divinity.hlspells.world.blocks.blockentities;
+
+import com.divinity.hlspells.setup.init.BlockInit;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class OrbOfEnchantingBE extends BlockEntity {
+
+    int xp = 0;
+    public OrbOfEnchantingBE(BlockPos pPos, BlockState pBlockState) {
+        super(BlockInit.ORB_BE.get(), pPos, pBlockState);
+    }
+
+    public void load(CompoundTag pTag) {
+        super.load(pTag);
+        this.xp = pTag.getInt("xp");
+    }
+
+    protected void saveAdditional(CompoundTag pTag) {
+        super.saveAdditional(pTag);
+        if (!(this.getXP() == 0)) {
+            pTag.putInt("xp", this.getXP());
+        }
+    }
+
+    public int getXP(){
+        return this.xp;
+    }
+    public void addXP(int xp){
+        this.xp += xp;
+        this.setChanged();
+    }
+    public void removeXP(int xp){
+        this.xp -= xp;
+        this.setChanged();
+    }
+
+    @Override
+    public BlockEntityType<?> getType() {
+        return BlockInit.ORB_BE.get();
+    }
+}
